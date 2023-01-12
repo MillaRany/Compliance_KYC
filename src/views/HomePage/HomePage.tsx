94ebe7406423640364f29cd840da0b679f811464
Milla Rany <milla_rany@hotmail.com>
2023-01-12 14:36:16 -0300

    first commit

---
+++ src/views/HomePage/HomePage.tsx
@@ -0,0 +1,22 @@
+import logo from "../../asset/img/Compliance.svg"
+import { Box, Image } from '@chakra-ui/react'
+import classes from "./HomePage.module.css";
+
+function HomePage() {
+
+  return (
+    <>
+      <Box className={classes.boxForm}>
+        <Box className={classes.boxForm} >
+          <Image
+            className="animate__animated animate__slideInRight "
+            src={logo}
+          />
+       
+      </Box>
+      </Box>
+    </>
+  )
+}
+
+export default HomePage
